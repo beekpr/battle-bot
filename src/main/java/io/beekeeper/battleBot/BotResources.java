f49648711436c958841624d459cf26db9f7cd8cf
wscsprint3r <stefan.irimescu@beekeeper.io>
2019-08-09 13:14:34 +0200

    conversation resources

---
+++ src/main/java/io/beekeeper/battleBot/BotResources.java
@@ -0,0 +1,14 @@
+package io.beekeeper.battleBot;
+
+public class BotResources {
+    public static final String INTRO = "Hi there! What can I help you with today? Here's what I can do:\n" +
+            "\n" +
+            "Type in /{{competitor's name}} to find more information about a specific competitor you are going up against\n" +
+            "If you would like to have a list of all our documented competitors, type in /list\n" +
+            "Have you come across a new competitor? Type in /add\n" +
+            "Use /{{contact's email adress}} to fetch everything related to the person you are meeting in your upcoming pitch";
+
+
+
+
+}
