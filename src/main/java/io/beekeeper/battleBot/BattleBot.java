1d5ab7464daae402811f25f5aaf9053d653a9d67
wscsprint3r <stefan.irimescu@beekeeper.io>
2019-08-09 11:02:10 +0200

    initial setup

--- src/main/java/io/beekeeper/battleBot/BattleBot.java
+++ src/main/java/io/beekeeper/battleBot/BattleBot.java
@@ -25,6 +25,7 @@ import com.google.api.services.sheets.v4.model.Request;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.UpdateCellsRequest;
 
+import com.google.api.services.sheets.v4.model.ValueRange;
 import io.beekeeper.core.BeekeeperApi;
 import io.beekeeper.battleBot.google.GoogleApiFactory;
 import io.beekeeper.battleBot.sheets.v1.AlreadyHandledException;
@@ -45,18 +46,39 @@ public class BattleBot extends ChatBot {
     private final DeveloperMetadataHelper metaData;
     private final BeekeeperApi api;
     private final String battleSheetId;
+    private final String SHEET_RANGE = "A2:E";
 
     public BattleBot(
             BeekeeperApi api,
             BeekeeperSDK sdk,
             GoogleApiFactory googleApiFactory,
             String sheetId
-    ) {
+    ) throws IOException {
         super(sdk);
         this.api = api;
         this.sheetsService = googleApiFactory.getSheetsService();
         this.metaData = new DeveloperMetadataHelper(sheetsService);
         this.battleSheetId = sheetId;
+        loadSheet();
+    }
+
+    private void loadSheet() throws IOException {
+        ValueRange response = sheetsService.spreadsheets().values()
+                .get(battleSheetId, SHEET_RANGE)
+                .execute();
+
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            System.out.println("Name, Major");
+            for (List row : values) {
+                // Print columns A and E, which correspond to indices 0 and 4.
+                System.out.printf("%s, %s\n", row.get(0), row.get(4));
+            }
+        }
+
+
     }
 
     @Override
