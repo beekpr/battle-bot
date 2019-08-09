b2911c7c5630a07c0cbe3a1c0bdb563a9bf333a4
wscsprint3r <stefan.irimescu@beekeeper.io>
2019-08-09 11:37:39 +0200

    Google Sheets connection WORKINGgit add src/git add src/!

--- src/main/java/io/beekeeper/battleBot/BattleBot.java
+++ src/main/java/io/beekeeper/battleBot/BattleBot.java
@@ -55,7 +55,6 @@ public class BattleBot extends ChatBot {
                                 rowString.append(row.get(i)).append(", ");
                             }
                             System.out.println(rowString);
-
                         }
                 );
             }
